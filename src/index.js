25535f140aa6b0ac4a225cb3e1a8cceee9f015b1
molestador <hustlerxxl@gmail.com>
2018-09-27 16:30:39 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zeros = 0;
+	while (number > 0) {
+	number /= 5;
+	zeros += Math.floor(number);
+}
+return	zeros;
 }
